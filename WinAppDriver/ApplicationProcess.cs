bf37eaa3f1c1da5904c0f7bb00c780efb6c3ce7d
v.khvorostianyi <v.khvorostianyi@temabit.com>
2019-04-05 14:38:34 +0300

    Remove comparetion with runing processes exe path. Because of access issue.

--- WinAppDriver/ApplicationProcess.cs
+++ WinAppDriver/ApplicationProcess.cs
@@ -5,6 +5,8 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace WinAppDriver
@@ -13,31 +15,25 @@ namespace WinAppDriver
     {
         public static Process StartProcessFromPath(string path, int timeout = 3000)
         {
-            // if existed the process with such path to the executable, then return it
-            var process = Process.GetProcesses()
-                .Where(x => x.MainModule.FileName == path).FirstOrDefault();
 
-            if (process == null)
+            var process = Process.Start(path);
+            process.WaitForInputIdle(timeout);
+            process.Refresh();
+            if (process.MainWindowHandle.ToInt32() == 0)
             {
-                process = Process.Start(path);
-                process.WaitForInputIdle(timeout);
-                process.Refresh();
-                if (process.MainWindowHandle.ToInt32() == 0)
+                int time = 0;
+                while (!process.HasExited)
                 {
-                    int time = 0;
-                    while (!process.HasExited)
+                    process.Refresh();
+                    if (process.MainWindowHandle.ToInt32() != 0)
                     {
-                        process.Refresh();
-                        if (process.MainWindowHandle.ToInt32() != 0)
-                        {
-                            return process;
-                        }
-                        Thread.Sleep(50);
-                        time += 50;
-                        if (time > timeout)
-                        {
-                            throw new TimeoutException("Process starting timeout expired.");
-                        }
+                        return process;
+                    }
+                    Thread.Sleep(50);
+                    time += 50;
+                    if (time > timeout)
+                    {
+                        throw new TimeoutException("Process starting timeout expired.");
                     }
                 }
             }
