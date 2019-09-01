971d3c69b7a9917bfb51f40e05d8364037d61102
Karel Frajták <kfrajtak@lekis.cz>
2019-09-01 21:48:37 +0200

    new extension class, new exception

---
+++ WinAppDriver/Exceptions/InvalidParameterValueException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAppDriver.Exceptions
+{
+    public class InvalidParameterValueException : Exception
+    {
+        public InvalidParameterValueException(string name, object value, Type type, Exception innerException)
+            : base($"InvalidParameterValueException {name} {value} {type}", innerException)
+        {
+
+        }
+    }
+}
---
+++ WinAppDriver/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using WinAppDriver.Exceptions;
+
+namespace WinAppDriver.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static T GetParameterValue<T>(this Dictionary<string, object> parameters, string parameterName)
+        {
+            if (!parameters.TryGetValue(parameterName, out var value))
+            {
+                throw new MissingParameterException(parameterName);
+            }
+
+            try
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidParameterValueException(parameterName, value, typeof(T), ex);
+            }
+        }
+
+        public static bool TryGetParameterValue<T>(this Dictionary<string, object> parameters, string parameterName, out T parameterValue)
+        {
+            parameterValue = default(T);
+
+            if (!parameters.TryGetValue(parameterName, out var value))
+            {
+                return false;
+            }
+
+            try
+            {
+                parameterValue = (T)Convert.ChangeType(value, typeof(T));
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
