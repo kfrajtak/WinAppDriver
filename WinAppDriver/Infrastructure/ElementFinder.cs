26d2829dcecfc076a9ba904d7ef54dfdbc1faa1e
Karel Frajták <kfrajtak@lekis.cz>
2019-09-01 21:47:39 +0200

    new find mechanisms (accessibility id, class name, name)

--- WinAppDriver/Infrastructure/ElementFinder.cs
+++ WinAppDriver/Infrastructure/ElementFinder.cs
@@ -16,10 +16,12 @@ namespace WinAppDriver.Infrastructure
 
     public class ElementFinder : IElementFinder
     {
-        private Regex _tagNameSelectorRegex = new Regex("^[A-Za-z]+$");
-        private Regex _elementIdSelectorRegex = new Regex("^#[A-Za-z0-9]+$");
+        private readonly Regex _tagNameSelectorRegex = new Regex("^[A-Za-z]+$");
+        private readonly Regex _elementIdSelectorRegex = new Regex("^#[A-Za-z0-9]+$");
         private readonly string _mechanism;
         private readonly string _criteria;
+
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
         public ElementFinder(string mechanism, string criteria)
         {
@@ -61,23 +63,29 @@ namespace WinAppDriver.Infrastructure
                     throw new InvalidSelectorException(mechanism + " " + criteria + ", only tag name and id selectors are allowed");
                 case "xpath":
                     break;
+                case "accessibility id":
+                    mechanism = "xpath";
+                    criteria = $"//*[@AutomationId='{criteria}']";
+                    break;
+                case "class name":
+                    mechanism = "xpath";
+                    criteria = $"//*[@ClassName='{criteria}']";
+                    break;
                 case "tag name":
                     mechanism = "xpath";
                     criteria = $"//{criteria}";
+                    break;
+                case "name":
+                    mechanism = "xpath";
+                    criteria = $"//*[@Name='{criteria}']";
                     break;
                 default:
                     throw new InvalidSelectorException(mechanism + " " + criteria);
             }
 
-            try
-            {
-                System.Diagnostics.Debug.WriteLine($"FindElements {mechanism} '{criteria}'...");
-                var walker = new AutomationElementTreeWalker(new XPathParser<IXPathExpression>().Parse(criteria, new WalkerBuilder()));
-                return walker.Find(automationElement, cancellationToken);
-            }
-            finally
-            {
-            }
+            Logger.Debug($"Find elements using {mechanism} and criteria '{criteria}' ...");
+            var walker = new AutomationElementTreeWalker(new XPathParser<IXPathExpression>().Parse(criteria, new WalkerBuilder()));
+            return walker.Find(automationElement, cancellationToken);
         }
     }
 }
