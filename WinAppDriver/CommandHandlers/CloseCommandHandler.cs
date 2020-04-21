8e388876d68af8e3ad67d788f26b27bb8b85d42a
kfrajtak <kfrajtak@gmail.com>
2020-04-21 17:49:45 +0200

    CloseCommandHandler returns an error response when window has been closed (hWnd = 0x0)

--- WinAppDriver/CommandHandlers/CloseCommandHandler.cs
+++ WinAppDriver/CommandHandlers/CloseCommandHandler.cs
@@ -14,6 +14,11 @@ namespace WinAppDriver.Server.CommandHandlers
         public override Response Execute(CommandEnvironment environment, Dictionary<string, object> parameters, System.Threading.CancellationToken cancellationToken)
         {
             var hwnd = environment.WindowHandle;
+            if (hwnd == System.IntPtr.Zero)
+            {
+                return Response.CreateErrorResponse(WebDriverStatusCode.NoSuchWindow, $"Currently selected window has been closed");
+            }
+
             var window = environment.Cache.AutomationElement;
             if (window.TryGetCurrentPattern(WindowPattern.Pattern, out object pattern) && pattern is WindowPattern windowPattern)
             {
